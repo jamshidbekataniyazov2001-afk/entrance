11d3699ab540e5033011121e3c70999ae2140aa9
jamshidbekataniyazov2001-afk <jamshidbekataniyazov2001@gmail.com>
2025-09-16 21:56:43 +0500

    Add files via upload

---
+++ juft yoki toq sonni aniqlash.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 14 21:59:47 2025
+
+@author: user
+"""
+
+son = input("Sonni kiriting: ")
+def juft_toq(son):
+    if int(son)%2==0:
+        print(f"{son} juft sonku")
+    else:
+        print(f"{son} toq sonku")
+print(juft_toq(son))
