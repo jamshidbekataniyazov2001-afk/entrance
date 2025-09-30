afa4a073d6306230bbf4543194710a003d9449dc
jamshidbekataniyazov2001-afk <jamshidbekataniyazov2001@gmail.com>
2025-09-30 07:03:32 +0500

    Add files via upload

---
+++ KATTA HARF BILAN YOZISH.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 18 20:34:31 2025
+
+@author: user
+"""
+def katta_harf(ismlar):
+    for i in range(len(ismlar)):
+        ismlar[i]= ismlar[i].title()
+ismlar = ['guli', 'aziz', 'asad', 'sardor', 'donyor']
+katta_harf(ismlar)
+print(ismlar)
+    
